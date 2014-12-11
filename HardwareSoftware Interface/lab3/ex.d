ff2a73e8ab3a6634f66bc586f9a340e23e4f3ca7
Varun Kapoor <vkatmu@gmail.com>
2014-12-11 19:29:20 +0530

    HWSWInterface

---
+++ HardwareSoftware Interface/lab3/ex.d
@@ -0,0 +1,11 @@
+
+ex.o:     file format elf64-x86-64
+
+
+Disassembly of section .text:
+
+0000000000000000 <.text>:
+   0:	48 b8 70 08 a6 75 e5 	movabs $0x6a9a2de575a60870,%rax
+   7:	2d 9a 6a 
+   a:	68 f3 0e 40 00       	pushq  $0x400ef3
+   f:	c3                   	retq   
---
+++ HardwareSoftware Interface/lab3/ex.s
@@ -0,0 +1,3 @@
+movabs $0x6a9a2de575a60870,%rax
+pushq  $0x400ef3
+retq
